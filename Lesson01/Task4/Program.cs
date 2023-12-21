ae58bd9629fe3edb0706c101c911585893f07a6f
Sergey Protopopov <protopopov_s@mail.ru>
2023-12-21 22:25:14 +0300

    add Lesson01/Task4

---
+++ Lesson01/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 4;
+int b = 3;
+int c = 8;
+int d = 2;
+int e = 5;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
