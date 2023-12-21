4165e3bd9ed73553a4ccfd94d64f42202b2fe613
Sergey Protopopov <protopopov_s@mail.ru>
2023-12-21 21:40:49 +0300

    add Lesson01/Task2

---
+++ Lesson01/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 3;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+
+long l1 = 11;
+long sum2 = l1 + i2;
+Console.WriteLine(sum2);
+
+double d1 = 11.222;
+double sum3 = d1 + i2;
+Console.WriteLine(sum3);
+
+int product1 = i1 * i2;
+long product2 = l1 * i2;
+double product3 = d1 * i2;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
