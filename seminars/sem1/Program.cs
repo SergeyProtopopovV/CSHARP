4e8c7f80c837619bae7ddf32c48d3c34d301c2a6
Sergey Protopopov <protopopov_s@mail.ru>
2024-01-18 21:03:16 +0300

    sem1

---
+++ seminars/sem1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Console.WriteLine("Input number: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine($"{num} в квадрате -> {num * num}");
+
+//напишите программу, которая принимает два числа и проверяет, является первое квадратом второго
+
+//Console.WriteLine("Input first number: ");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input second number: ");
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//if (num2 * num2 == num1)
+//{
+//    Console.WriteLine($"{num1} является квадратом {num2}");
+//} else
+//{
+//    Console.WriteLine($"{num1} не является квадратом {num2}");
+//}
+
+//напишите программу, которая принимает на вход целое N, а на выходе показывает все целые от -N до N
+
+Console.WriteLine("Input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = -n; i <= n; i++)
+{
+    Console.Write(i + " ");
+}
